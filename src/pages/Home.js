f50a6df530e5821d278dbf1b2c2fabb6b808e214
unknown <alexhparsons@gmail.com>
2023-07-17 14:53:55 -0700

    third

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import backgroundhome from '../img/backgroundhome.png'
+import logo from '../img/Vector.png'
 
 function Home() {
   return (
@@ -7,15 +8,37 @@ function Home() {
       className="h-screen bg-no-repeat bg-fixed bg-center bg-cover"
       style={{ backgroundImage: `url(${backgroundhome})` }}
     >
-      <header>
-        <div className='flex flex-row justify-center gap-24'>
-            <div>blah</div>
-            <div>blah</div>
-            <div>blah</div>
-            <div>blah</div>
-            <div>blah</div>
+    <header class="flex justify-between py-4 mx-auto max-w-4xl text-white font-sans tracking-wide">
+      <img src={logo} />
+        <div class="flex justify-center gap-24 text-lg">
+            <div>Profile</div>
+            <div>Favorites</div>
+            <div>My Books</div>
         </div>
-      </header>
+        <div className='text-lg'>Sign In</div>
+    </header>
+
+    <section class="grid grid-cols-9 gap-9 mx-auto max-w-6xl">
+    <div class="w-14 h-24 bg-gray-300"></div>
+    <div class="w-14 h-24 bg-gray-300"></div>
+    <div class="w-14 h-24 bg-gray-300"></div>
+    <div class="w-14 h-24 bg-gray-300"></div>
+    <div class="w-14 h-24 bg-gray-300"></div>
+    <div class="w-14 h-24 bg-gray-300"></div>
+    <div class="w-14 h-24 bg-gray-300"></div>
+    <div class="w-14 h-24 bg-gray-300"></div>
+    <div class="w-14 h-24 bg-gray-300"></div>
+    <div class="w-14 h-24 bg-gray-300"></div>
+    <div class="w-14 h-24 bg-gray-300"></div>
+    <div class="w-14 h-24 bg-gray-300"></div>
+    <div class="w-14 h-24 bg-gray-300"></div>
+    <div class="w-14 h-24 bg-gray-300"></div>
+    <div class="w-14 h-24 bg-gray-300"></div>
+    <div class="w-14 h-24 bg-gray-300"></div>
+    <div class="w-14 h-24 bg-gray-300"></div>
+    <div class="w-14 h-24 bg-gray-300"></div>
+    
+    </section>
     </div>
   )
 }
