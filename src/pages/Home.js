144ae335c0598a811d269fec29dd34ec2088aef8
unknown <alexhparsons@gmail.com>
2023-07-23 17:20:51 -0700

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import plug from '../img/plus.png'
 import Header from '../components/header.js'
 import { Link } from 'react-router-dom'
 import { useAuth0 } from '@auth0/auth0-react'
+import Sidebar from '../components/sidebar.js'
 
 
 
@@ -15,29 +16,32 @@ function Home() {
     const [data, setData] = useState([]);
 
     useEffect(() => {
+        console.log('useEffect called');
         fetch('http://localhost:3001/users')
-          .then(response => response.json())
-          .then(data => setData(data))
-          .catch(error => console.error('An error occurred:', error));
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data, 'd211d');
+            setData(data);
+          })
+          .catch((error) => console.error('An error occurred:', error));
       }, []);
-
-    console.log(data)
-
+      
+      console.log(data, 'this is appear')
   return (
     <div
-      className="h-screen bg-no-repeat bg-fixed bg-center bg-cover"
-      style={{ backgroundImage: `url(${backgroundhome})` }}
+      className="bg-black"
+    //   style={{ backgroundImage: `url(${backgroundhome})` }}
     >
-    
-    <Header />
 
-    <section className='mx-auto max-w-6xl'>
+    <Sidebar dataFromParent={data} />
+
+    {/* <section className='mx-auto max-w-6xl'>
     <h1 className='text-center text-white text-3xl pt-20 pb-14'>All Books</h1>
-    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-8 gap-y-12 bottom-0 justify-items-center">
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-8 gap-y-6 bottom-0 justify-items-center">
         {data.map((item, index) => (
             <div
             key={item.title}
-            className={`w-28 h-40 bg-gray-300 ${
+            className={`w-48 h-40 bg-gray-300 ${
                 index >= 3 && index < 5 ? 'hidden sm:block' : ''
             } ${index >= 5 && index < 7 ? 'hidden md:block' : ''} ${
                 index >= 7 && index < 9 ? 'hidden lg:block' : ''
@@ -58,7 +62,7 @@ function Home() {
             )}
         </div>
         </div>
-    </section>
+    </section> */}
 
     <footer className='fixed bottom-0 w-full flex flex-row gap-x-24 justify-center h-[50px] text-gray-200 opacity-30'>
         <div>FAQ</div>
